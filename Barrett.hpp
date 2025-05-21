a60040768412a4cc220310279f8f24c08d9f867c
huxint <1348168485@qq.com>
2025-05-21 15:31:41 +0800

    edit Barrett

--- Barrett.hpp
+++ Barrett.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include <algorithm>
+#include <cstdint>
 
 class Barrett32 {
 private:
-    using u32 = unsigned;
-    using u64 = unsigned long long;
+    using u32 = std::uint32_t;
+    using u64 = std::uint64_t;
+    using u128 = unsigned __int128;
 
 public:
-    constexpr Barrett32(u32 _mod) {
-        setmod(_mod);
-    }
+    constexpr Barrett32(u32 _mod) : modular(_mod), inverse(static_cast<u64>(-1) / _mod + 1) {}
 
     constexpr auto setmod(u32 _mod) -> void {
         modular = _mod;
@@ -22,7 +22,7 @@ public:
 
     constexpr auto multiply(u32 lhs, u32 rhs) const -> u32 {
         u64 value = static_cast<u64>(lhs) * rhs;
-        u32 res = static_cast<u32>(value - static_cast<u64>((static_cast<unsigned __int128>(value) * inverse) >> 64) * mod());
+        u32 res = static_cast<u32>(value - static_cast<u64>((static_cast<u128>(value) * inverse) >> 64) * mod());
         return res >= mod() ? res += mod() : res;
     }
 
