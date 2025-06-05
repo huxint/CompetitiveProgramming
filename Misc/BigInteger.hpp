d7b05dd7627535c2fd36c3a31f8124f9dd13fcc8
huxint <huxint123@gmail.com>
2025-06-05 22:08:10 +0800

    edit

--- Misc/BigInteger.hpp
+++ Misc/BigInteger.hpp
@@ -16,9 +16,9 @@ protected:
     static constexpr std::size_t karatsuba_width = 6;
     static constexpr std::size_t simple_mulpty_limit = 1024;
     static constexpr std::size_t simple_inverse_limit = 16;
-    static constexpr std::array<std::size_t, 10> pow10 = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
-    static constexpr std::size_t base = pow10[width];
-    static constexpr std::size_t karatsuba_base = pow10[karatsuba_width];
+    static constexpr std::array<std::uint32_t, 10> pow10 = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+    static constexpr std::uint32_t base = pow10[width];
+    static constexpr std::uint32_t karatsuba_base = pow10[karatsuba_width];
 
 public:
     constexpr BigInteger() {
@@ -55,7 +55,7 @@ public:
         }
         digits.clear();
         sign = value < 0 ? -1 : value != 0;
-        std::size_t x = value < 0 ? -value : value;
+        auto x = value < 0 ? -value : value;
         do {
             digits.push_back(x % base);
             x /= base;
@@ -71,16 +71,16 @@ public:
         digits.clear();
         std::size_t start = not std::isdigit(value.front());
         sign = start ? value.front() == '-' ? -1 : 1 : 1;
-        std::size_t size = (value.size() - start + width - 1) / width;
-        digits.resize(size--);
+        std::size_t _size = (value.size() - start + width - 1) / width;
+        digits.resize(_size--);
         auto point = value.data() + start;
         auto end = value.data() + value.size();
-        std::size_t remainder = (value.size() - start) % width;
-        if (remainder) {
-            std::from_chars(point, point + remainder, digits[size--]);
+        std::size_t rem = (value.size() - start) % width;
+        if (rem) {
+            std::from_chars(point, point + rem, at(--_size));
         }
-        for (point += remainder; point != end; point += width) {
-            std::from_chars(point, point + width, digits[size--]);
+        for (point += rem; point != end; point += width) {
+            std::from_chars(point, point + width, at(--_size));
         }
         normalize();
         return *this;
@@ -121,7 +121,7 @@ public:
     constexpr std::string to_binary(bool reverse = true) const {
         std::string res;
         for (BigInteger value(*this); value; value /= 2) {
-            res += (value.digits.front() & 1) | 0X30;
+            res += (value.at(0) & 1) | 0X30;
         }
         if (reverse) {
             std::reverse(res.begin(), res.end());
@@ -131,11 +131,11 @@ public:
 
     constexpr std::string to_string() const {
         std::string res;
-        res.reserve(width_size() * width + 1);
+        res.reserve(size() * width + 1);
         if (sign == -1) {
             res += '-';
         }
-        res += std::to_string(digits.back());
+        res += std::to_string(at(size() - 1));
         std::string str;
         for (auto value : digits | std::views::reverse | std::views::drop(1)) {
             str = std::to_string(value);
@@ -255,18 +255,18 @@ public:
             return *this = other;
         }
         sign = sign * other.sign;
-        digits = width_size() * other.width_size() > simple_mulpty_limit ? karatsuba_multiply(digits, other.digits) : simple_multiply(digits, other.digits);
+        digits = size() * other.size() > simple_mulpty_limit ? karatsuba_multiply(digits, other.digits) : simple_multiply(digits, other.digits);
         normalize();
         return *this;
     }
 
     constexpr BigInteger &operator/=(std::int64_t other) {
         if (other == 2) {
-            for (std::size_t i = width_size() - 1; i + 1 != 0; --i) {
-                if ((digits[i] & 1) and i != 0) {
-                    digits[i - 1] += base;
+            for (std::size_t i = size(); i-- != 0;) {
+                if ((at(i) & 1) and i != 0) {
+                    at(i - 1) += base;
                 }
-                digits[i] >>= 1;
+                at(i) >>= 1;
             }
             normalize();
         } else {
@@ -278,7 +278,7 @@ public:
     constexpr BigInteger &operator%=(std::int64_t other) {
         if (other == 2) {
             digits.erase(digits.begin() + 1, digits.end());
-            digits.front() &= 1;
+            at(0) &= 1;
         } else {
             // Todo
         }
@@ -335,12 +335,10 @@ public:
     friend constexpr std::strong_ordering operator<=>(const BigInteger &lhs, const BigInteger &rhs) {
         if (lhs.sign != rhs.sign) {
             return lhs.sign <=> rhs.sign;
-        } else if (lhs.zero()) {
+        } else if (lhs) {
             return std::strong_ordering::equal;
-        } else if (lhs.sign == 1) {
-            return lhs.compare_abs(rhs);
         } else {
-            return rhs.compare_abs(lhs);
+            return lhs.sign == 1 ? lhs.compare_abs(rhs) : rhs.compare_abs(lhs);
         }
     }
 
@@ -375,12 +373,20 @@ private:
     int sign;
     std::vector<std::uint32_t> digits;
 
-    constexpr std::size_t width_size() const {
+    constexpr std::uint32_t &at(std::size_t index) {
+        return digits.at(index);
+    }
+
+    constexpr std::uint32_t at(std::size_t index) const {
+        return digits.at(index);
+    }
+
+    constexpr std::size_t size() const {
         return digits.size();
     }
 
     constexpr std::strong_ordering compare_abs(const BigInteger &other) const {
-        return width_size() != other.width_size() ? width_size() <=> other.width_size() : std::lexicographical_compare_three_way(digits.rbegin(), digits.rend(), other.digits.rbegin(), other.digits.rend());
+        return size() != other.size() ? size() <=> other.size() : std::lexicographical_compare_three_way(digits.rbegin(), digits.rend(), other.digits.rbegin(), other.digits.rend());
     }
 
     constexpr void normalize() {
@@ -396,7 +402,7 @@ private:
         std::string lhs = to_binary(false);
         std::string rhs = other.to_binary(false);
         *this = 0;
-        for (std::size_t i = std::max(lhs.size(), rhs.size()) - 1; i + 1 != 0; --i) {
+        for (std::size_t i = std::max(lhs.size(), rhs.size()); i-- != 0;) {
             *this += *this;
             *this += op(i < lhs.size() ? (lhs[i] & 1) : 0, i < rhs.size() ? (rhs[i] & 1) : 0);
         }
@@ -406,23 +412,23 @@ private:
     constexpr void plus_impl(const BigInteger &other) {
         bool carry = false;
         std::size_t i = 0;
-        std::size_t size = other.width_size();
-        if (width_size() < size) {
-            digits.resize(size);
+        std::size_t _size = other.size();
+        if (size() < _size) {
+            digits.resize(_size);
         }
-        for (; i < size; ++i) {
-            digits[i] += other.digits[i] + carry;
-            if (carry = digits[i] >= base; carry) {
-                digits[i] -= base;
+        for (; i < _size; ++i) {
+            at(i) += other.at(i) + carry;
+            if (carry = at(i) >= base; carry) {
+                at(i) -= base;
             }
         }
         for (; carry; ++i) {
-            if (i == width_size()) {
+            if (i == size()) {
                 digits.push_back(0);
             }
-            digits[i] += carry;
-            if (carry = digits[i] >= base; carry) {
-                digits[i] -= base;
+            at(i) += carry;
+            if (carry = at(i) >= base; carry) {
+                at(i) -= base;
             }
         }
         normalize();
@@ -430,14 +436,14 @@ private:
 
     constexpr void minus_impl(const BigInteger &other) {
         bool carry = false;
-        auto current = digits.data();
-        std::size_t size = other.width_size();
-        for (std::size_t i = 0; i < size or carry; ++i, ++current) {
-            std::size_t value = carry + (i < size ? other.digits[i] : 0);
-            if (carry = value > *current; carry) {
-                *current -= value - base;
+        auto it = digits.data();
+        std::size_t _size = other.size();
+        for (std::size_t i = 0; i < _size or carry; ++i, ++it) {
+            std::size_t value = carry + (i < _size ? other.at(i) : 0);
+            if (carry = value > *it; carry) {
+                *it -= value - base;
             } else {
-                *current -= value;
+                *it -= value;
             }
         }
         normalize();
@@ -446,8 +452,9 @@ private:
     constexpr std::vector<std::uint32_t> simple_multiply(const std::vector<std::uint32_t> &lhs, const std::vector<std::uint32_t> &rhs) {
         std::vector<std::uint32_t> res(lhs.size() + rhs.size());
         for (std::size_t i = 0; i < lhs.size(); ++i) {
-            for (std::size_t j = 0, carry = 0; (lhs[i] != 0 and j < rhs.size()) or carry != 0; ++j) {
-                res[i + j] = (carry += res[i + j] + std::size_t(lhs[i]) * (j < rhs.size() ? rhs[j] : 0)) % base;
+            std::uint64_t carry = 0;
+            for (std::size_t j = 0; (lhs[i] != 0 and j < rhs.size()) or carry != 0; ++j) {
+                res[i + j] = (carry += res[i + j] + std::uint64_t(lhs[i]) * (j < rhs.size() ? rhs[j] : 0)) % base;
                 carry /= base;
             }
         }
@@ -459,7 +466,7 @@ private:
 
     static constexpr std::vector<std::uint32_t> convert_base(const std::vector<std::uint32_t> &vector, std::size_t old_width, std::size_t new_width) {
         std::vector<std::uint32_t> res;
-        std::size_t carry = 0;
+        std::uint64_t carry = 0;
         for (std::size_t i = 0, _width = 0; i < vector.size(); ++i) {
             carry += vector[i] * pow10[_width];
             for (_width += old_width; _width >= new_width; _width -= new_width) {
@@ -483,17 +490,17 @@ private:
         x.resize(max);
         y.resize(max);
         auto karatsuba = [](auto &&self, const std::vector<std::uint64_t> &lhs, const std::vector<std::uint64_t> &rhs) -> std::vector<std::uint64_t> {
-            std::size_t size = lhs.size();
-            std::vector<std::uint64_t> res(2 * size);
-            if (size <= 32) {
-                for (std::size_t i = 0; i < size; ++i) {
-                    for (std::size_t j = 0; lhs[i] != 0 and j < size; ++j) {
+            std::size_t _size = lhs.size();
+            std::vector<std::uint64_t> res(2 * _size);
+            if (_size <= 32) {
+                for (std::size_t i = 0; i < _size; ++i) {
+                    for (std::size_t j = 0; lhs[i] != 0 and j < _size; ++j) {
                         res[i + j] += lhs[i] * rhs[j];
                     }
                 }
                 return res;
             }
-            std::size_t split = size / 2;
+            std::size_t split = _size / 2;
             std::vector<std::uint64_t> lhs_low(lhs.begin(), lhs.begin() + split);
             std::vector<std::uint64_t> lhs_high(lhs.begin() + split, lhs.end());
             std::vector<std::uint64_t> rhs_low(rhs.begin(), rhs.begin() + split);
@@ -511,7 +518,7 @@ private:
             }
             for (std::size_t i = 0; i < high.size(); ++i) {
                 mid[i] -= high[i];
-                res[i + size] += high[i];
+                res[i + _size] += high[i];
             }
             for (std::size_t i = 0; i < mid.size(); ++i) {
                 res[i + split] += mid[i];
