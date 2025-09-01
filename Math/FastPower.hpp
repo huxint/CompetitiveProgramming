9c750bfd2c1b7712bd2b6215efcb4fd52ddf3144
huxint <huxint123@gmail.com>
2025-09-01 16:24:53 +0800

    add: constexpr

--- Math/FastPower.hpp
+++ Math/FastPower.hpp
@@ -8,7 +8,7 @@
 #include <cstdint>
 #include <concepts>
 #include <limits>
-auto power(auto base, std::size_t exp) {
+constexpr auto power(auto base, std::size_t exp) {
     decltype(base) res(1);
     for (; exp != 0; exp >>= 1, base *= base) {
         if (exp & 1) {
@@ -18,7 +18,7 @@ auto power(auto base, std::size_t exp) {
     return res;
 }
 
-auto power(std::integral auto base, std::size_t exp, const auto &modular) {
+constexpr auto power(std::integral auto base, std::size_t exp, const auto &modular) {
     using usize = std::make_unsigned_t<decltype(base)>;
     using ulsize = std::conditional_t<(std::numeric_limits<usize>::digits < 64), std::uint64_t, __uint128_t>;
     ulsize res = 1;
