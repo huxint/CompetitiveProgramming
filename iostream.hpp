661eee440c0a88b382232e2c38f49358a4fe37a4
huxint <1348168485@qq.com>
2025-09-28 23:39:40 +0800

    refactor: iostream.hpp

--- iostream.hpp
+++ iostream.hpp
@@ -1,12 +1,15 @@
+/**
+ * @brief 输入输出的重载
+ */
 #pragma once
 #include <string_view>
 #include <ranges>
 
 namespace iostream {
     template <typename T>
-    concept range_like = std::ranges::range<T> and not std::is_convertible_v<T, std::string_view>;
+    concept range_like = std::ranges::range<T> && !std::is_convertible_v<T, std::string_view>;
     template <typename T>
-    concept range_tuple = range_like<T> or std::__is_tuple_like<T>::value and not std::ranges::range<T>;
+    concept range_tuple = range_like<T> || std::__is_tuple_like<T>::value && !std::ranges::range<T>;
 
     template <typename Istream>
     Istream &operator>>(Istream &istream, range_tuple auto &value) {
@@ -15,9 +18,12 @@ namespace iostream {
                 istream >> e;
             }
         } else {
+            // clang-format off
+            // 因为我觉得 这样写更清晰一些 clang-format 感觉差点意思
             std::apply([&istream](auto &&...args) {
                 ((istream >> args), ...);
             }, value);
+            // clang-format on
         }
         return istream;
     }
@@ -30,12 +36,14 @@ namespace iostream {
                 ostream << ' ' << *it;
             }
         } else {
+            // clang-format off
             ostream << '(' << (std::apply([&ostream](auto &&...args) {
                 auto size = sizeof...(args);
                 ((ostream << args << (--size ? ", " : "")), ...);
             }, value), ')');
+            // clang-format on
         }
         return ostream;
     }
-}
+} // namespace iostream
 using iostream::operator<<, iostream::operator>>;
